f57e8fc9cbbc89200d987d81ab989f1c696b8014
Glen Marker <glencfl@gmail.com>
2021-04-15 11:53:02 -0400

    feat: remove conflicting draft releases automatically within autorelease

--- .github/actions/autorelease/src/index.ts
+++ .github/actions/autorelease/src/index.ts
@@ -11,6 +11,15 @@ import { env } from "process";
 
 type OctoKit = ReturnType<typeof github.getOctokit>;
 
+interface GithubRelease {
+	id: number;
+	upload_url: string;
+	html_url: string;
+	tag_name: string;
+	body: string;
+	target_commitish: string;
+}
+
 interface GithubConfig {
 	token: string;
 	sha: string;
@@ -67,18 +76,34 @@ async function existingReleaseDeletion(octo: OctoKit, config: GithubConfig): Pro
 	}
 }
 
+async function existingDraftDeletion(octo: OctoKit, config: GithubConfig): Promise<void> {
+	const [ owner, repo ] = config.repository.split("/");
+	const iterator = octo.paginate.iterator<GithubRelease>(octo.repos.listReleases.endpoint.merge({ per_page: 100,
+		owner, repo }));
+
+	let release: GithubRelease | undefined;
+	for await (const response of iterator) {
+		let result = response.data.find(release => release.tag_name === config.reference);
+		if (result) {
+			release = result;
+			break;
+		}
+	}
+
+	if (release != null) {
+		await timeout(apiTimeout);
+		octo.repos.deleteRelease({ owner, repo, release_id: release.id });
+	}
+}
+
 async function main() {
 	const config = getConfig();
 	if (config == null) return;
 
-	let octo: OctoKit;
-	if (config.token == null) {
-		core.setFailed("Action was not given a token.");
-		return;
-	} else {
-		octo = github.getOctokit(config.token);
-	}
-
+	const octo = github.getOctokit(config.token);
+	await timeout(apiTimeout);
+	await existingDraftDeletion(octo, config);
+	await timeout(apiTimeout);
 	await existingReleaseDeletion(octo, config);
 	console.log("Good times.");
 }
